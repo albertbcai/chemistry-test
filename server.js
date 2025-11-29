ca3254a056d1ac2a6b595426fe570dd64713300d
albertbcai <albertbcai@gmail.com>
2025-11-29 13:28:22 -0500

    Update server to listen on 0.0.0.0 for Railway

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Oligonucleotide Calculator server running on port ${PORT}`);
 });
 
